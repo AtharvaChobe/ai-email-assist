b9dfb1b14bce9890fa9f825c37a7ca76bc75c07d
AtharvaChobe <103590543+AtharvaChobe@users.noreply.github.com>
2025-09-30 13:29:20 +0530

    first commit

--- emailAssist/src/main/java/com/emailAssist/emailAssist/controller/EmailGeneratorController.java
+++ emailAssist/src/main/java/com/emailAssist/emailAssist/controller/EmailGeneratorController.java
@@ -5,10 +5,7 @@ import com.emailAssist.emailAssist.service.EmailRequestService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +13,11 @@ public class EmailGeneratorController {
 
     @Autowired
     private EmailRequestService emailRequestService;
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "ok";
+    }
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateResponse(@RequestBody EmailRequest request){
