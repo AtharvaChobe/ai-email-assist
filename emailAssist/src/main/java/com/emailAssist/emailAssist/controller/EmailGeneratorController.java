130860cdea2469cc1660157a91a3a83eca33203d
AtharvaChobe <103590543+AtharvaChobe@users.noreply.github.com>
2025-09-30 16:17:24 +0530

    made ping route public

--- emailAssist/src/main/java/com/emailAssist/emailAssist/controller/EmailGeneratorController.java
+++ emailAssist/src/main/java/com/emailAssist/emailAssist/controller/EmailGeneratorController.java
@@ -9,19 +9,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://mail.google.com")
 public class EmailGeneratorController {
 
     @Autowired
     private EmailRequestService emailRequestService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/ping")
-    public String ping(){
+    public String ping() {
         return "ok";
     }
 
+    @CrossOrigin(origins = "https://mail.google.com")
     @PostMapping("/generate")
-    public ResponseEntity<String> generateResponse(@RequestBody EmailRequest request){
+    public ResponseEntity<String> generateResponse(@RequestBody EmailRequest request) {
         System.out.println("   \n sending request     ");
         String response = emailRequestService.generateEmailReply(request);
         return ResponseEntity.ok(response);
